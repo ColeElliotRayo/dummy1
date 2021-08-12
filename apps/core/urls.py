d703046a7c8be0ebcaa48b68ef8fd5a2b300225a
Cole Rayo <cole.rayo@gmail.com>
2021-08-11 17:39:00 -0700

    butchering

--- apps/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from apps.core import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('chart/', views.chart, name='chart'),
-    path('graph/', views.graph, name='graph'),
-]
